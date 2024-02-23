477b30c7b679fcb051af739119b30bdfc061db46
Prempal Singh <prempal.42@gmail.com>
2024-02-23 19:53:20 +0530

    update Sdk version to build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ly.img.editor.matrix"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "ly.img.editor.matrix"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
